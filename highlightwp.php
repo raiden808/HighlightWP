93d17ff68263af727cf6bc54681dac654f89f1f5
raiden <raiden.nalasa@gmail.com>
2019-02-16 11:37:23 +0800

    initial commit

---
+++ highlightwp.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: HighlightWP
+ * Plugin URI: 
+ * Description: Highlight and share similar to medium.
+ * Author: <a href="http://reydennalasa.com">Rey Den</a>
+ * Text Domain: high_wp
+ * Domain Path: /languages
+ * Version: 1.0.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+//* Defined constant
+define( 'HLWP_TEXTDOMAIN', 'high_wp' );
+define( 'HLWP_VERSION', '0.0.0' );
+define( 'HLWP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'HLWP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
